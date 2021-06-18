11f221a9319f46740ac1fbb99d24b5625da2e7c8
David Fuentes <fuentesdt@gmail.com>
2021-06-18 15:46:41 -0500

    WIP:

--- lrstatistics.sql
+++ lrstatistics.sql
@@ -12,38 +12,29 @@ select InstanceUID,SegmentationID,FeatureID,LabelID,Mean,StdD,Max,Min,cast(Count
 
 create table volumestats  as
 select lt.InstanceUID,1  LabelID,
-            max(CASE WHEN lt.SegmentationID='lrtrain-1.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=3 THEN lt.Count ELSE NULL END) countlr3,
-            max(CASE WHEN lt.SegmentationID='lrtrain-1.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=4 THEN lt.Count ELSE NULL END) countlr4,
-            max(CASE WHEN lt.SegmentationID='lrtrain-1.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=5 THEN lt.Count ELSE NULL END) countlr5,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID= 'lrtrain.nii.gz' and lt.LabelID=1 THEN lt.Count ELSE NULL END) compsize 
-from lstat lt
-GROUP BY   lt.InstanceUID
-UNION
-select lt.InstanceUID,2  LabelID,
-            max(CASE WHEN lt.SegmentationID='lrtrain-2.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=3 THEN lt.Count ELSE NULL END) countlr3,
-            max(CASE WHEN lt.SegmentationID='lrtrain-2.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=4 THEN lt.Count ELSE NULL END) countlr4,
-            max(CASE WHEN lt.SegmentationID='lrtrain-2.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=5 THEN lt.Count ELSE NULL END) countlr5,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID= 'lrtrain.nii.gz' and lt.LabelID=2 THEN lt.Count ELSE NULL END) compsize 
-from lstat lt
-GROUP BY   lt.InstanceUID
-UNION
-select lt.InstanceUID,3  LabelID,
-            max(CASE WHEN lt.SegmentationID='lrtrain-3.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=3 THEN lt.Count ELSE NULL END) countlr3,
-            max(CASE WHEN lt.SegmentationID='lrtrain-3.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=4 THEN lt.Count ELSE NULL END) countlr4,
-            max(CASE WHEN lt.SegmentationID='lrtrain-3.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=5 THEN lt.Count ELSE NULL END) countlr5,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID= 'lrtrain.nii.gz' and lt.LabelID=3 THEN lt.Count ELSE NULL END) compsize 
+            max(CASE WHEN lt.SegmentationID='Truth.raw-1.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=1 THEN lt.Count ELSE NULL END) countlr1,
+            max(CASE WHEN lt.SegmentationID='Truth.raw-1.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=2 THEN lt.Count ELSE NULL END) countlr2,
+            max(CASE WHEN lt.SegmentationID='Truth.raw.nii.gz' and FeatureID= 'Truth.raw.nii.gz' and lt.LabelID=1 THEN lt.Count ELSE NULL END) compsize 
 from lstat lt
 GROUP BY   lt.InstanceUID
 ORDER BY   lt.InstanceUID  ASC;
+-- UNION
+-- select lt.InstanceUID,2  LabelID,
+--             max(CASE WHEN lt.SegmentationID='Truth.raw-2.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=3 THEN lt.Count ELSE NULL END) countlr3,
+--             max(CASE WHEN lt.SegmentationID='Truth.raw-2.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=4 THEN lt.Count ELSE NULL END) countlr4,
+--             max(CASE WHEN lt.SegmentationID='Truth.raw-2.nii.gz' and FeatureID = 'lirads.nii.gz' and lt.LabelID=5 THEN lt.Count ELSE NULL END) countlr5,
+--             max(CASE WHEN lt.SegmentationID='Truth.raw.nii.gz' and FeatureID= 'Truth.raw.nii.gz' and lt.LabelID=2 THEN lt.Count ELSE NULL END) compsize 
+-- from lstat lt
+-- GROUP BY   lt.InstanceUID
+-- ORDER BY   lt.InstanceUID  ASC;
 
 
 create table widelstat  as
 select lt.InstanceUID,cast(lt.LabelID as int) LabelID,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID = 'EPM_3.nii.gz' THEN lt.Mean ELSE NULL END)  EPM,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID = 'lirads-3.nii.gz' THEN lt.Mean ELSE NULL END)  predict3,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID = 'lirads-4.nii.gz' THEN lt.Mean ELSE NULL END)  predict4,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID = 'lirads-5.nii.gz' THEN lt.Mean ELSE NULL END)  predict5,
-            max(CASE WHEN lt.SegmentationID='lrtrain.nii.gz' and FeatureID = 'lrtrain.nii.gz'  THEN lt.Mean ELSE NULL END)  truth,
+            max(CASE WHEN lt.SegmentationID='Truth.raw.nii.gz' and FeatureID = 'Art.raw.nii.gz' THEN lt.Mean ELSE NULL END)  ART,
+            max(CASE WHEN lt.SegmentationID='Truth.raw.nii.gz' and FeatureID = 'lirads-1.nii.gz' THEN lt.Mean ELSE NULL END)  predict1,
+            max(CASE WHEN lt.SegmentationID='Truth.raw.nii.gz' and FeatureID = 'lirads-2.nii.gz' THEN lt.Mean ELSE NULL END)  predict2,
+            max(CASE WHEN lt.SegmentationID='Truth.raw.nii.gz' and FeatureID = 'Truth.raw.nii.gz'  THEN lt.Mean ELSE NULL END)  truth,
             lt.Count
 from lstat lt
 where lt.LabelID !=0
@@ -52,5 +43,5 @@ ORDER BY   lt.InstanceUID  ASC, cast(lt.LabelID as int) ASC;
 
 
 .output qastats/wide.csv  
-select substr(ws.InstanceUID,1,7)  ptid, ws.*, vs.* from volumestats vs join  widelstat ws  on vs.InstanceUID = ws.InstanceUID and vs.LabelID = ws.LabelID  where vs.compsize > 20;
+select ws.InstanceUID  ptid, ws.*, vs.* from volumestats vs join  widelstat ws  on vs.InstanceUID = ws.InstanceUID and vs.LabelID = ws.LabelID ;
 .quit
