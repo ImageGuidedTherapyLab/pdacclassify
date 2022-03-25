83a54c7ee0ce2b71ef2031ac0575849c62efc3c4
David Fuentes <fuentesdt@gmail.com>
2022-03-25 11:58:14 -0500

    WIP:

--- pdacgemcitabine.sql
+++ pdacgemcitabine.sql
@@ -31,12 +31,13 @@ attach database 'dicom/ctkDICOM.sql' as sl;
 
 
 create table flagdata  as
-select sd.PatientsUID,pt.PatientID,im.StudyInstanceUID,im.SeriesInstanceUID,im.seriesDescription,im.SeriesDate,
+select sd.PatientsUID,pt.PatientID,im.StudyInstanceUID,im.SeriesInstanceUID,im.seriesDescription,im.SeriesDate,di.FileName,
 CASE WHEN (im.seriesDescription == "PreXRT-PRE  resampled" or im.seriesDescription == "PRE LIVER resampled" or im.seriesDescription == "PostXRT-Pre resampled" or im.seriesDescription == "PreXRT-Pre resampled" or im.seriesDescription == "PRE resampled" or im.seriesDescription == "PreXRT-PRE resampled")  THEN 'Pre'
      WHEN (im.seriesDescription like '%Port%' or im.seriesDescription like '%port%' or im.seriesDescription like '%ven%'or im.seriesDescription like '%PV%' )  THEN 'Ven'
      WHEN (im.seriesDescription == 'normal pancreas' or im.seriesDescription == 'Series Description' or im.seriesDescription == 'SeriesDescription') THEN 'Truth'
      ELSE 'Art' END AS ImageType
 from sl.series im 
+join sl.images di on di.SeriesInstanceUID= im.SeriesInstanceUID
 join sl.studies  sd on sd.StudyInstanceUID = im.StudyInstanceUID
 join sl.patients pt on sd.PatientsUID = pt.UID;
 -- select * from flagdata where ImageType = 'Ven';
@@ -50,15 +51,35 @@ select *, 'Truth'||truthid truthlabel  from labeldatatmp;
 create table widestudy  as
 select fg.PatientsUID PatientsUID,fg.PatientID PatientID,ed.DeltaScore,fg.StudyInstanceUID StudyInstanceUID,
             max(CASE WHEN fg.ImageType = 'Pre' THEN fg.SeriesInstanceUID       ELSE NULL END)  Pre,
+            max(CASE WHEN fg.ImageType = 'Pre' THEN fg.SeriesDescription       ELSE NULL END)  PreDescription,
+            max(CASE WHEN fg.ImageType = 'Pre' THEN rtrim(fg.Filename, replace(fg.Filename, '/', '')) ELSE NULL END) PreFilename,
             max(CASE WHEN fg.ImageType = 'Art' THEN fg.SeriesInstanceUID       ELSE NULL END)  Art,
+            max(CASE WHEN fg.ImageType = 'Art' THEN fg.SeriesDescription       ELSE NULL END)  ArtDescription,
+            max(CASE WHEN fg.ImageType = 'Art' THEN rtrim(fg.Filename, replace(fg.Filename, '/', '')) ELSE NULL END) ArtFilename,
             max(CASE WHEN fg.ImageType = 'Ven' THEN fg.SeriesInstanceUID       ELSE NULL END)  Ven,
+            max(CASE WHEN fg.ImageType = 'Ven' THEN fg.SeriesDescription       ELSE NULL END)  VenDescription,
+            max(CASE WHEN fg.ImageType = 'Ven' THEN rtrim(fg.Filename, replace(fg.Filename, '/', '')) ELSE NULL END) VenFilename,
             max(CASE WHEN ld.truthlabel = 'Truth1' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth1,
+            max(CASE WHEN ld.truthlabel = 'Truth1' THEN ld.SeriesDescription     ELSE NULL END)  Truth1Description,
+            max(CASE WHEN ld.truthlabel = 'Truth1' THEN ld.Filename              ELSE NULL END)  Truth1FileName,
             max(CASE WHEN ld.truthlabel = 'Truth2' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth2,
+            max(CASE WHEN ld.truthlabel = 'Truth2' THEN ld.SeriesDescription     ELSE NULL END)  Truth2Description,
+            max(CASE WHEN ld.truthlabel = 'Truth2' THEN ld.Filename              ELSE NULL END)  Truth2FileName,
             max(CASE WHEN ld.truthlabel = 'Truth3' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth3,
+            max(CASE WHEN ld.truthlabel = 'Truth3' THEN ld.SeriesDescription     ELSE NULL END)  Truth3Description,
+            max(CASE WHEN ld.truthlabel = 'Truth3' THEN ld.Filename              ELSE NULL END)  Truth3FileName,
             max(CASE WHEN ld.truthlabel = 'Truth4' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth4,
+            max(CASE WHEN ld.truthlabel = 'Truth4' THEN ld.SeriesDescription     ELSE NULL END)  Truth4Description,
+            max(CASE WHEN ld.truthlabel = 'Truth4' THEN ld.Filename              ELSE NULL END)  Truth4FileName,
             max(CASE WHEN ld.truthlabel = 'Truth5' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth5,
+            max(CASE WHEN ld.truthlabel = 'Truth5' THEN ld.SeriesDescription     ELSE NULL END)  Truth5Description,
+            max(CASE WHEN ld.truthlabel = 'Truth5' THEN ld.Filename              ELSE NULL END)  Truth5FileName,
             max(CASE WHEN ld.truthlabel = 'Truth6' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth6,
-            max(CASE WHEN ld.truthlabel = 'Truth7' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth7
+            max(CASE WHEN ld.truthlabel = 'Truth6' THEN ld.SeriesDescription     ELSE NULL END)  Truth6Description,
+            max(CASE WHEN ld.truthlabel = 'Truth6' THEN ld.Filename              ELSE NULL END)  Truth6FileName,
+            max(CASE WHEN ld.truthlabel = 'Truth7' THEN ld.SeriesInstanceUID     ELSE NULL END)  Truth7,
+            max(CASE WHEN ld.truthlabel = 'Truth7' THEN ld.SeriesDescription     ELSE NULL END)  Truth7Description,
+            max(CASE WHEN ld.truthlabel = 'Truth7' THEN ld.Filename              ELSE NULL END)  Truth7FileName
 from flagdata  fg
 join edrn      ed on   ed.MRN=fg.PatientID
 join labeldata      ld on   ld.PatientID=fg.PatientID
@@ -77,23 +98,6 @@ where ws.Art is null;
 -- wide format
 .mode csv
 .output dicom/wideformatd2.csv 
-select ws.*,dn.seriesDescription PreDescription , an.seriesDescription ArtDescription, pt.seriesDescription VenDescription, tt.seriesDescription Truth1Description, td.seriesDescription Truth2Description,
-rtrim(di.Filename, replace(di.Filename, '/', '')) PreFilename,
-rtrim(ai.Filename, replace(ai.Filename, '/', '')) ArtFilename,
-rtrim(pi.Filename, replace(pi.Filename, '/', '')) VenFilename,
-                           ti.Filename            Truth1Filename,
-                           t2.Filename            Truth2Filename
-from widestudy ws 
-join sl.images  di on di.SeriesInstanceUID= ws.Pre
-join sl.images  ai on ai.SeriesInstanceUID= ws.Art
-join sl.images  pi on pi.SeriesInstanceUID= ws.Ven
-join sl.images  ti on ti.SeriesInstanceUID= ws.Truth1
-join sl.images  t2 on t2.SeriesInstanceUID= ws.Truth2
-join flagdata  dn on dn.SeriesInstanceUID= ws.Pre
-join flagdata  an on an.SeriesInstanceUID= ws.Art
-join flagdata  pt on pt.SeriesInstanceUID= ws.Ven 
-join flagdata  tt on tt.SeriesInstanceUID= ws.Truth1
-join flagdata  td on td.SeriesInstanceUID= ws.Truth2
-GROUP BY    ws.StudyInstanceUID;
+select ws.* from widestudy ws ;
 
 .quit
